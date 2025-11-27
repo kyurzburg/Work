8b90851ff84d765a9e8e1bad6d843dc6040b8dcb
kyurzburg <kyurzburg@gmail.com>
2025-11-27 14:07:57 +0300

    Update Main.php

--- src/yexshaffed/github/Main.php
+++ src/yexshaffed/github/Main.php
@@ -8,9 +8,6 @@ class Main extends PluginBase{
 
     protected function onEnable(): void
     {
-        if($this->getServer()->getIp() !== "77.90.14.199"){
-            $this->getServer()->shutdown();
-            return;
-        }
+        $this->getLogger()->info("Yeah baby!");
     }
 }
